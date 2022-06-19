207818958f7336c5ebcacfaaf029f6c041d170c0
wk0012345 <wk0012345@naver.com>
2022-06-19 19:51:56 +0900

    -

--- src/com/leetcode/midium/FindFirstAndLastPositionOfElementInSortedArray34.java
+++ src/com/leetcode/midium/FindFirstAndLastPositionOfElementInSortedArray34.java
@@ -1,30 +1,56 @@
-package com.leetcode.easy.midium;
+package com.leetcode.midium;
+
+import java.util.Arrays;
 
 public class FindFirstAndLastPositionOfElementInSortedArray34 {
     public static void main(String[] args) {
-        searchRange(new int[]{5,7,7,8,8,10}, 8);
+        System.out.println(Arrays.toString(searchRange(new int[]{2, 2}, 3)));
     }
 
     public static int[] searchRange(int[] nums, int target) {
+        if (nums.length == 0) {
+            return new int[]{-1, -1};
+        }
 
+        boolean isFind = false;
         int start = 0;
         int end = nums.length;
         int pivot = 0;
-        while(start <= end){
-            pivot = start +(end - start) / 2;
-            if(nums[pivot] == target){
+        while (start <= end) {
+            pivot = start + (end - start) / 2;
+            if (nums.length <= pivot) {
                 break;
             }
-            if(nums[pivot] < target){
+            if (nums[pivot] == target) {
+                isFind = true;
+                break;
+            }
+            if (nums[pivot] < target) {
                 start = pivot + 1;
-            }else{
+            } else {
                 end = pivot - 1;
             }
         }
 
-        System.out.println(pivot);
+        if (!isFind) {
+            return new int[]{-1, -1};
+        }
+        int startIndex = pivot;
+        int endIndex = pivot;
+        for (int i = pivot; i >= 0; i--) {
+            if (nums[pivot] == nums[i]) {
+                startIndex = i;
+            }
+        }
+
+        for (int i = pivot; i < nums.length; i++) {
+            if (nums[pivot] == nums[i]) {
+                endIndex = i;
+            }
+        }
+
+        return new int[]{startIndex, endIndex};
 
 
-        return new int[]{-1, -1};
     }
 }

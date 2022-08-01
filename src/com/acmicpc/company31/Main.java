1fcbe0b8f112c530be3090a0d679cafb1387dd72
wk0012345 <wk0012345@naver.com>
2022-08-02 00:32:50 +0900

    -

--- src/com/acmicpc/company31/Main.java
+++ src/com/acmicpc/company31/Main.java
@@ -7,33 +7,29 @@ import java.util.StringTokenizer;
 
 // 텀 프로젝트
 public class Main {
-    public static int testCase;
+
+    public static int N;
     public static int[] arr;
     public static boolean[] visited;
     public static boolean[] finished;
-
     public static int count;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        testCase = Integer.parseInt(br.readLine());
-
+        int testCase = Integer.parseInt(br.readLine());
         for (int tc = 0; tc < testCase; tc++) {
-            int N = Integer.parseInt(br.readLine());
+            N = Integer.parseInt(br.readLine());
             arr = new int[N + 1];
             visited = new boolean[N + 1];
             finished = new boolean[N + 1];
             count = 0;
             StringTokenizer st = new StringTokenizer(br.readLine());
-
-            for (int i = 1; i < N + 1; i++) {
+            for (int i = 1; i <= N; i++) {
                 arr[i] = Integer.parseInt(st.nextToken());
             }
 
-            for (int i = 1; i < N + 1; i++) {
-                if (!finished[i]) {
-                    dfs(i);
-                }
+            for (int i = 1; i <= N; i++) {
+                dfs(i);
             }
             System.out.println(N - count);
         }
@@ -41,19 +37,22 @@ public class Main {
 
     public static void dfs(int now) {
         if (visited[now]) {
-            finished[now] = true;
-            count++;
-        } else {
-            visited[now] = true;
+            return;
         }
+        visited[now] = true;
         int next = arr[now];
 
-        if (!finished[next]) {
+        if (!visited[next]) {
             dfs(next);
+        } else {
+            if (!finished[next]) {
+                count++;
+                for (int origin = next; origin != now; origin = arr[origin]) {
+                    count++;
+                }
+            }
         }
-        visited[now] = false;
         finished[now] = true;
     }
 }
-
 // https://bcp0109.tistory.com/32

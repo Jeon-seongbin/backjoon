253b2a610ffb0c36aefc83438198b4e7674ae736
wk0012345 <wk0012345@naver.com>
2022-10-16 16:40:21 +0900

    first

---
+++ src/com/leetcode/midium/UniqueBinarySearchTrees96.java
@@ -0,0 +1,25 @@
+package com.leetcode.midium;
+
+public class UniqueBinarySearchTrees96 {
+    public static void main(String[] args) {
+
+    }
+
+    public int numTrees(int n) {
+        if (n < 2) {
+            return 1;
+        }
+
+        int[] result = new int[n + 1];
+
+        result[0] = 1;
+        result[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                result[i] = result[i] + result[j] * result[i - j - 1];
+            }
+        }
+        return result[n];
+    }
+}

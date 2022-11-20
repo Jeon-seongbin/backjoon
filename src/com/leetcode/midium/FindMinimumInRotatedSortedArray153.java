ca9fc9f8e190449da388ab170f329b8358619386
wk0012345 <wk0012345@naver.com>
2022-11-20 14:38:52 +0900

    leetcode

--- src/com/leetcode/midium/FindMinimumInRotatedSortedArray153.java
+++ src/com/leetcode/midium/FindMinimumInRotatedSortedArray153.java
@@ -7,7 +7,31 @@ public class FindMinimumInRotatedSortedArray153 {
     }
 
     public static int findMin(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] > nums[end]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return nums[start];
+    }
+
+    public static int findMin2(int[] nums) {
         Arrays.sort(nums);
         return nums[0];
+    }
+
+    public int findMin3(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        for (int num : nums) {
+            if (num < min)
+                min = num;
+        }
+        return min;
     }
 }

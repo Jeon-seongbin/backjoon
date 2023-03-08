96df9576e866af0169bc241f75c31ba9ef67ae27
jeon <wk0012345@naver.com>
2023-03-08 22:54:39 +0900

    leetcode

--- src/com/leetcode/midium/KokoEatingBananas875.java
+++ src/com/leetcode/midium/KokoEatingBananas875.java
@@ -5,28 +5,36 @@ public class KokoEatingBananas875 {
 
     }
 
-    public static int minEatingSpeed(int[] piles, int h) {
+    public int minEatingSpeed(int[] piles, int h) {
         int left = 1;
-        int right = 1000000000;
+        int right = 0;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (check(piles, mid, h)) right = mid - 1;
-            else left = mid + 1;
+        for (int p : piles) {
+            right = Math.max(right, p);
         }
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (check(piles, mid, h)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
         return left;
     }
 
-    public static boolean check(int[] piles, int k, int h) {
-
+    public boolean check(int[] piles, int mid, int h) {
         int hours = 0;
-        for (int pile : piles) {
-            int div = pile / k;
-            hours += div;
-            if (pile % k != 0) {
+        for (int p : piles) {
+            int time = p / mid;
+            hours += time;
+            if (p % mid != 0) {
                 hours++;
             }
         }
         return hours <= h;
     }
+
 }

eebef3a01cca8a9edd5eb546fb4658da1ce126be
jeon <wk0012345@naver.com>
2023-06-23 00:36:40 +0900

    programmers

--- src/com/leetcode/midium/BestTimeToBuyAndSellStockQithTransactionFee714.java
+++ src/com/leetcode/midium/BestTimeToBuyAndSellStockQithTransactionFee714.java
@@ -6,13 +6,24 @@ public class BestTimeToBuyAndSellStockQithTransactionFee714 {
     }
 
     public static int maxProfit(int[] prices, int fee) {
-        int sell = 0;
-        int buy = prices[0];
+        // 현재 돈
+        int cash = 0;
+
+        // 가지고있는 주식
+        int prevStockPrice = prices[0];
+
         for (int i = 1; i < prices.length; i++) {
-            buy = Math.max(buy, prices[i] - sell - fee);
-            sell = Math.min(sell, prices[i] - buy);
+
+            // 현재 주식가격에서 - 가지고있는 주식을 팔고 - 수수료 까지 떼면 돈이 나오는데
+            // 최댓값을 구한다
+            cash = Math.max(cash, prices[i] - prevStockPrice - fee);
+
+            // 과거 주식 가격에서 가장 낮은것을 구한다
+            prevStockPrice = Math.min(prevStockPrice, prices[i] - cash);
+
         }
-        return sell;
+
+        return cash;
     }
 
 }

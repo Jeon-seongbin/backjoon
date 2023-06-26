d4e80696393bc8784268fd0210381f5378fd4c91
jeon <wk0012345@naver.com>
2023-06-27 00:09:12 +0900

    programmers

--- src/com/programmers/level2/PatriotMissile.java
+++ src/com/programmers/level2/PatriotMissile.java
@@ -6,27 +6,21 @@ public class PatriotMissile {
     public static void main(String[] args) {
 
         solution(new int[][]{
-                {4, 5},
-                {4, 8},
-                {10, 14},
-                {11, 13},
-                {5, 12},
-                {3, 7},
-                {1, 4}
+                {0, 1},
+                {5, 4},
+                {1, 4},
+                {2, 3},
         });
     }
 
     public static int solution(int[][] targets) {
         int answer = 0;
-
         Arrays.sort(targets, (o1, o2) -> (o1[1] - o2[1]));
-
-        int before = 0;
-
+        int kijun = 0;
         for (int i = 0; i < targets.length; i++) {
-            if (targets[i][0] >= before) {
+            if (kijun <= targets[i][0]) {
                 answer++;
-                before = targets[i][1];
+                kijun = targets[i][1];
             }
         }
         return answer;

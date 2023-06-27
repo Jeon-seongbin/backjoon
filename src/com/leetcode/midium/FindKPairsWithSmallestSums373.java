d05e38a59402ff26e7ff6b7477ce14332bacf3c5
jeon <wk0012345@naver.com>
2023-06-27 23:48:23 +0900

    leetcode

---
+++ src/com/leetcode/midium/FindKPairsWithSmallestSums373.java
@@ -0,0 +1,32 @@
+package com.leetcode.midium;
+
+import java.util.*;
+
+public class FindKPairsWithSmallestSums373 {
+    public static void main(String[] args) {
+        kSmallestPairs(new int[]{1}, new int[]{1, 100, 111}, 5);
+    }
+
+    public static List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> (a[0] + a[1]) - (b[0] + b[1]));
+
+        for (int i = 0; i < nums1.length && i < k; i++) {
+            minHeap.add(new int[]{nums1[i], nums2[0], 0});
+        }
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < k && !minHeap.isEmpty(); i++) {
+
+            int[] curr = minHeap.poll();
+            result.add(List.of(curr[0], curr[1]));
+
+            int nums2Idx = curr[2];
+            if (nums2Idx < nums2.length - 1) {
+                minHeap.add(new int[]{curr[0], nums2[nums2Idx + 1], nums2Idx + 1});
+            }
+        }
+
+        return result;
+    }
+}

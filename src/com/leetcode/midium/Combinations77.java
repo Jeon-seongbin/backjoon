d8afdb2c431bd623d6146ca4a72767f0a3b78253
jeon <wk0012345@naver.com>
2023-08-12 16:08:23 +0900

    leetcode

--- src/com/leetcode/midium/Combinations77.java
+++ src/com/leetcode/midium/Combinations77.java
@@ -4,25 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Combinations77 {
-    public static void main(String[] args) {
-        combine(4, 2);
-    }
-    public static List<List<Integer>> combine(int n, int k) {
+    public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> result = new ArrayList<>();
         helper(1, result, new ArrayList<>(), n, k);
         return result;
     }
 
-    public static void helper(int start, List<List<Integer>> result, List<Integer> combine, int n, int k) {
-        if (combine.size() == k) {
-            result.add(new ArrayList<>(combine));
+    public void helper(int startIndex, List<List<Integer>> result, List<Integer> combination, int n, int k) {
+        if (combination.size() == k) {
+            result.add(new ArrayList<>(a));
             return;
         }
 
-        for (int i = start; i <= n; i++) {
-            combine.add(i);
-            helper(i + 1, result, combine, n, k);
-            combine.remove(combine.size() - 1);
+        for (int i = startIndex; i <= n; i++) {
+            combination.add(i);
+            helper(i + 1, result, combination, n, k);
+            combination.remove(combination.size() - 1);
         }
+    }
+
+    public static void main(String[] args) {
     }
 }

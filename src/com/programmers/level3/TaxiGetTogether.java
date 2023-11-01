f61ddebc992597efd38ce9e3bb993b8d2b1ca2fc
jeon <wk0012345@naver.com>
2023-11-02 01:47:32 +0900

    programmers

--- src/com/programmers/level3/TaxiGetTogether.java
+++ src/com/programmers/level3/TaxiGetTogether.java
@@ -4,132 +4,87 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.PriorityQueue;
 
-class Edge implements Comparable<Edge> {
-    int index;
-    int cost;
-
-    Edge(int index, int cost) {
-        this.index = index;
-        this.cost = cost;
-    }
-
-    @Override
-    public int compareTo(Edge e) {
-        return this.cost - e.cost;
-    }
-}
-
 public class TaxiGetTogether {
-    public static void main(String[] args) {
+    static class Node implements Comparable<Node> {
+        int cost;
+        int nodeNumber;
 
+        Node(int nodeNumber,  int cost ) {
+            this.nodeNumber = nodeNumber;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return cost - o.cost;
+        }
     }
 
-    static final int MAX = 20000001;
+    public static ArrayList<ArrayList<Node>> graph;
+    public static int MAX = Integer.MAX_VALUE;
 
-    static ArrayList<ArrayList<Edge>> graph;
-
-    public int solution(int n, int s, int a, int b, int[][] fares) {
+    public static int solution(int n, int s, int a, int b, int[][] fares) {
         int answer = Integer.MAX_VALUE;
-
         graph = new ArrayList<>();
-
-        for (int i = 0; i < n + 1; i++) {
-            graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
         }
 
         for (int i = 0; i < fares.length; i++) {
-            graph.get(fares[i][0]).add(new Edge(fares[i][1], fares[i][2]));
-            graph.get(fares[i][1]).add(new Edge(fares[i][0], fares[i][2]));
+            graph.get(fares[i][0]).add(new Node(fares[i][1], fares[i][2]));
+            graph.get(fares[i][1]).add(new Node(fares[i][0], fares[i][2]));
         }
-
         int[] whenstartACost = new int[n + 1];
         int[] whenstartBCost = new int[n + 1];
         int[] whenstartCost = new int[n + 1];
-
         Arrays.fill(whenstartACost, MAX);
         Arrays.fill(whenstartBCost, MAX);
         Arrays.fill(whenstartCost, MAX);
 
         whenstartACost = dijkstra(a, whenstartACost);
-        whenstartBCost = dijkstra(a, whenstartBCost);
-        whenstartCost = dijkstra(a, whenstartCost);
+        whenstartBCost = dijkstra(b, whenstartBCost);
+        whenstartCost = dijkstra(s, whenstartCost);
 
-
-        for (int i = 0; i < n + 1; i++) {
+        for (int i = 0; i <= n; i++) {
             answer = Math.min(answer, whenstartACost[i] + whenstartBCost[i] + whenstartCost[i]);
         }
+
         return answer;
     }
 
-    static int[] dijkstra(int start, int[] costs) {
+    public static int[] dijkstra(int startPoint, int[] costs) {
 
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        pq.offer(new Edge(start, 0));
-        costs[start] = 0;
+        PriorityQueue<Node> q = new PriorityQueue<>();
+        q.add(new Node(startPoint, 0));
+        costs[startPoint] = 0;
 
-        while (!pq.isEmpty()) {
-            Edge nowPoint = pq.poll();
-            int nowIndex = nowPoint.index;
-            int nowCost = nowPoint.cost;
+        while (!q.isEmpty()) {
+            Node nowNode = q.poll();
+            int nowIndex = nowNode.nodeNumber;
+            int nowCost = nowNode.cost;
 
             if (costs[nowIndex] < nowCost) {
                 continue;
             }
 
-            ArrayList<Edge> nextEdges = graph.get(nowIndex);
+            ArrayList<Node> nextEdges = graph.get(nowIndex);
 
-            for (Edge edge : nextEdges) {
+            for (Node edge : nextEdges) {
 
                 int nextCost = costs[nowIndex] + edge.cost;
-                if (nextCost < costs[edge.index]) {
-                    costs[edge.index] = nextCost;
-                    pq.offer(new Edge(edge.index, nextCost));
+                if (  nextCost< costs[edge.nodeNumber]) {
+                    costs[edge.nodeNumber] = nextCost;
+                    q.offer(new Node(edge.nodeNumber, nextCost));
                 }
             }
-
         }
         return costs;
     }
 
-    public int solution1(int n, int s, int a, int b, int[][] fares) {
-        int answer = 0;
+    public static void main(String[] args) {
+        solution(6, 4, 6, 2, new int[][]
+                {{4, 1, 10}, {3, 5, 24}, {5, 6, 2}, {3, 1, 41}, {5, 1, 24}, {4, 6, 50}, {2, 4, 66}, {2, 3, 22}, {1, 6, 25}}
+        );
 
-        int[][] dp = new int[n + 1][n + 1];
-        int max = 20000001;
-
-        for (int i = 0; i < n + 1; i++) {
-            for (int j = 0; j < n + 1; j++) {
-                if (i == j) {
-                    dp[i][j] = 0;
-                    continue;
-                }
-                dp[i][j] = max;
-            }
-        }
-
-        for (int i = 0; i < fares.length; i++) {
-            int start = fares[i][0];
-            int end = fares[i][1];
-            int cost = fares[i][2];
-            dp[start][end] = cost;
-            dp[end][start] = cost;
-        }
-
-        for (int z = 0; z < n + 1; z++) {
-            for (int i = 0; i < n + 1; i++) {
-                for (int j = 0; j < n + 1; j++) {
-                    dp[i][j] = Math.min(dp[i][j], dp[i][z] + dp[z][j]);
-                }
-            }
-        }
-
-
-        answer = dp[s][a] + dp[s][b];
-
-        for (int i = 0; i < n + 1; i++) {
-            answer = Math.min(answer, dp[s][i] + dp[i][a] + dp[i][b]);
-        }
-
-        return answer;
     }
 }

eb7182d1e007a33890924adfcab4dee933bf8ccc
jeon <wk0012345@naver.com>
2024-07-19 19:28:33 +0900

    test

--- src/com/codingTest/lobot/Main.java
+++ src/com/codingTest/lobot/Main.java
@@ -3,35 +3,82 @@ package com.codingTest.lobot;
 import java.util.*;
 
 public class Main {
-    public static String longestCommonPrefix(List<String> words) {
+    static class TrieNode {
+
+        private String result;
+        private HashMap<Character, TrieNode> trieNode;
+        private boolean isEndOfWord;
+
+        TrieNode() {
+            this.trieNode = new HashMap<>();
+            this.isEndOfWord = false;
+            this.result = "";
+        }
+
+        public String getResult() {
+            return result;
+        }
+    }
+
+    static class Trie {
+        private final TrieNode root;
+
+        public Trie() {
+            root = new TrieNode();
+        }
+
+
+        public void insert(String word) {
+            TrieNode current = root;
+            for (char w : word.toCharArray()) {
+                current = current.trieNode.computeIfAbsent(w, ww -> new TrieNode());
+            }
+            current.isEndOfWord = true;
+            root.result = word;
+        }
+
+
+        public boolean startWith(String prefix) {
+            TrieNode current = root;
+            String temp = "";
+            for (char p : prefix.toCharArray()) {
+                current = current.trieNode.get(p);
+                if (current == null) {
+                    root.result = temp;
+                    return false;
+                }
+                temp += p;
+            }
+            root.result = temp;
+            return true;
+        }
+    }
+
+
+    public static String commonPrefix(List<String> words) {
+
         if (words == null || words.isEmpty()) {
             return "";
         }
 
-       Collections.sort(words);
+        Trie trie = new Trie();
 
-        String first = words.get(0);
-        String last = words.get(words.size() - 1);
+        trie.insert(words.get(0));
 
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < first.length() && i < last.length(); i++) {
-            if (first.charAt(i) != last.charAt(i)) {
-                break;
-            }
-            result.append(first.charAt(i));
+        for (int i = 1; i < words.size(); i++) {
+            trie.startWith(words.get(i));
         }
-
-        return result.toString();
+        return trie.root.getResult();
     }
 
 
     public static void main(String[] args) {
-        System.out.println(longestCommonPrefix(Arrays.asList("flower", "flow", "flight")));   // -> "fl"
-        System.out.println(longestCommonPrefix(Arrays.asList("flow", "flower", "flowchart")));   // -> "flow"
-        System.out.println(longestCommonPrefix(Arrays.asList("flower", "flow", "flowchart")));   // -> "flow"
-        System.out.println(longestCommonPrefix(Arrays.asList("car", "cat", "dog", "bird", "cow", "rabbit")));   // -> ""
-        System.out.println(longestCommonPrefix(Arrays.asList("one word")));   // -> "one word"
-        System.out.println(longestCommonPrefix(Arrays.asList("")));   // -> ""
-        System.out.println(longestCommonPrefix(Arrays.asList()));   // -> ""
+        System.out.println(commonPrefix(Arrays.asList("flower", "flow", "flight")));   // -> "fl"
+        System.out.println(commonPrefix(Arrays.asList("flow", "flower", "flowchart")));   // -> "flow"
+        System.out.println(commonPrefix(Arrays.asList("flower", "flow", "flowchart")));   // -> "flow"
+        System.out.println(commonPrefix(Arrays.asList("car", "cat", "dog", "bird", "cow", "rabbit")));   // -> ""
+        System.out.println(commonPrefix(Arrays.asList("one word")));   // -> "one word"
+        System.out.println(commonPrefix(Arrays.asList("")));   // -> ""
+        System.out.println(commonPrefix(Arrays.asList()));   // -> ""
     }
 }
--- src/com/leetcode/midium/ImnplementTrie208.java
+++ src/com/leetcode/midium/ImnplementTrie208.java
@@ -6,20 +6,22 @@ public class ImnplementTrie208 {
     public static void main(String[] args) {
         Trie trie = new Trie();
         trie.insert("abc");
+        boolean result = trie.startsWith("ab");
+        System.out.println(result);
     }
 
+
     static class TrieNode {
-        HashMap<Character, TrieNode> childen;
+        HashMap<Character, TrieNode> trieNode;
         boolean isEndOfWord;
 
         TrieNode() {
-            this.childen = new HashMap<>();
-            isEndOfWord = false;
+            this.trieNode = new HashMap<>();
+            this.isEndOfWord = false;
         }
     }
 
     static class Trie {
-
         private final TrieNode root;
 
         public Trie() {
@@ -27,33 +29,39 @@ public class ImnplementTrie208 {
         }
 
         public void insert(String word) {
+            // Objectが作成された時点のRootを持ってくる
             TrieNode current = root;
-            for (char ch : word.toCharArray()) {
-                current = current.childen.computeIfAbsent(ch, c -> new TrieNode());
+
+            for (char w : word.toCharArray()) {
+                // wが存在しなかったら、wをTreeの最後に追加する
+                // 最後のは常に最後になる
+                current = current.trieNode.computeIfAbsent(w, ww -> new TrieNode());
             }
             current.isEndOfWord = true;
         }
 
-        public boolean search(String word) {
-            TrieNode current = root;
-            for (char ch : word.toCharArray()) {
-                current = current.childen.get(ch);
-                if (current == null) {
-                    return false;
-                }
-            }
-            return current.isEndOfWord;
-        }
-
         public boolean startsWith(String prefix) {
             TrieNode current = root;
-            for (char ch : prefix.toCharArray()) {
-                current = current.childen.get(ch);
+            for (char p : prefix.toCharArray()) {
+                current = current.trieNode.get(p);
                 if (current == null) {
                     return false;
                 }
             }
             return true;
+        }
+
+
+        public boolean search(String word) {
+            TrieNode current = root;
+
+            for (char w : word.toCharArray()) {
+                current = current.trieNode.get(w);
+                if (current == null) {
+                    return false;
+                }
+            }
+            return current.isEndOfWord;
         }
     }
 }
